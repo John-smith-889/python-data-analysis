92d0c2ab166e38435af11a5789d44a844c43c433
Smith <bartlomiejczajewski@gmail.com>
2020-03-08 00:42:18 +0100

    add classification assessment section

--- data-modelling.py
+++ data-modelling.py
@@ -40,3 +40,28 @@ model_01.fit(train_X, train_target)
 
 # Predict values on test set
 Y_pred_01 = model_01.predict(test_X)
+
+
+
+#=============================================================================#
+# Classification model assessment #
+#=================================#
+
+from sklearn.metrics import confusion_matrix
+
+# Distinguish unique values of target_test variable
+unique, counts = np.unique(test_target, return_counts=True)
+for i in range(0,len(unique)):
+    print(unique[i], ": ", counts[i])
+# categories are printed in order of future confusion matrix
+
+
+
+
+
+
+
+
+
+
+
